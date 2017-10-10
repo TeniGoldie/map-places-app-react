b319f1b6290bb8075653f2d1a120db0df5ee88c6
TeniGoldie <ta.zharska@gmail.com>
2017-10-10 18:53:05 +0300

    added the google map with location

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Places from './Places';
 
 class App extends Component {
   render() {
+    const location = {
+      lat: 35.652832, lng: 139.83947
+    }
     return (
       <div className="App">
         Welcome to React
-        <Map />
+        <div style={{width:100+'%', height: 400, background: 'lightblue'}}>
+          <Map
+            center={{lat:35.7082, lng: 139.7305}}
+            zoom={14}
+            containerElement={<div style={{height:100+'%'}} />}
+            mapElement={<div style={{height:100+'%'}} />}
+          />
+        </div>
         <Places />
       </div>
     );
